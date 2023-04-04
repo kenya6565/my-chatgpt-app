71ccc451cd235da572bffa853d9e0b2821ad9997
kenya6565 <ken.australia0605@gmail.com>
2023-04-05 01:41:05 +0900

    move chat.ts under api directory

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -3,9 +3,16 @@ import axios from 'axios';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { data } = await axios.post('https://api.openai.com/v1/engine/davinci-codex/completions', {
+    // text sent to API
     prompt: 'Hello, ',
+
+    // max length of text which API generates
     max_tokens: 60,
+
+    // the number of text which API generates
     n: 1,
+
+    // end keyword API generates
     stop: ['\n'],
   }, {
     headers: {
